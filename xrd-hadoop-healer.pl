2f90c12dde08b9a94cd3ec90a714ef2170dc90f6
Matevž Tadel <osschar@users.noreply.github.com>
2012-10-04 06:04:08 +0000

    Prepare preliminary cmd-line for hadoop repair invocation.

--- xrd-hadoop-healer.pl
+++ xrd-hadoop-healer.pl
@@ -6,13 +6,16 @@
 
 @DIRS = qw( /store/mc );
 
-$GETBB = "./get_bad_blocks";
-$XRDFC = "./xrdfragcp";
+$GETBB  = "./get_bad_blocks";
+$XRDFC  = "./xrdfragcp";
+$REPAIR = "hadoop jar hdtools.jar repair";
 
 $HDP_PREFIX  = "/cms/phedex";
 $XRD_PREFIX  = "root://xrootd.unl.edu/";
 $XRD_POSTFIX = "?tried=xrootd.t2.ucsd.edu";
 
+$FRAGMENT_NAME   = "fragment";
+$FIXED_FILE_NAME = "fixed-file";
 
 #-----------------------------------------------------------------------
 # Main program
@@ -31,28 +34,43 @@ for $dir (@DIRS)
     my @els = split ' ', $bfl;
 
     my $hdp_file = shift @els;
-    die "Hadoop file $hdp_file does not start with $HDP_PREFIX." unless
-	$hdp_file =~ m/^$HDP_PREFIX/;
+    my ($basename) = $hdp_file =~ m!/([^/]+)$! or
+	die "Can not extract basename from $hdp_file";
+    $hdp_file =~ m/^$HDP_PREFIX/ or
+	die "Hadoop file $hdp_file does not start with $HDP_PREFIX.";
     my $xrd_file = $hdp_file;
     $xrd_file =~ s/^$HDP_PREFIX//;
     $xrd_file = $XRD_PREFIX . $xrd_file . $XRD_POSTFIX;
 
     my @frag_args;
+    my @repair_args;
     while (my $frag = shift @els)
     {
-      die "Wrong fragment format '$frag'" unless
-	  $frag =~ m/^(\d+),(\d+)$/;
+      $frag =~ m/^(\d+),(\d+)$/ or
+	  die "Wrong fragment format '$frag'";
       push @frag_args, "$1 $2";
+      push @repair_args, $frag;
     }
 
     my $xrdfc_cmd = join(" ", $XRDFC,
+			      "--prefix", $FRAGMENT_NAME,
                               map({ "--frag $_" } @frag_args),
                               $xrd_file);
     print "Executing: $xrdfc_cmd\n";
     system $xrdfc_cmd and die "Execution of $xrdfc_cmd failed.";
 
-    # run file-fixer
-    print "I'd execute the file-fixer now if I knew how ...\n";
+    my $repair_cmd = join("", $REPAIR,
+                              "--prefix", $FRAGMENT_NAME,
+                              "--outfile", $FIXED_FILE_NAME,
+                              $hdp_file);
+    print "Executing: $repair_cmd\n";
+    # MT: This doesn't quite work yet ....
+    system $repair_cmd and die "Execution of $repair_cmd failed.";
+
+    # Register the new file back ...
+    print "Now should register $FIXED_FILE_NAME as $basename ...\n";
+
+    # And remove fragments, fixed-file
   }
   close BFS;
 }
